6ca2fb3980cf7de04f18c9daa5650d57b76dc557
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-27 17:29:24 -0700

    add question erb files and add "add a question in this survey" link

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'surveys#index'
   resources :surveys do
-    resource :questions
+    resources :questions
   end
 end
 
